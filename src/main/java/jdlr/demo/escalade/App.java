1315434ec85120e528c596e6d014a0250c3e20d3
JonathanDLR <marie89>
2019-10-06 13:00:25 +0200

    first commit

---
+++ src/main/java/jdlr/demo/escalade/App.java
@@ -0,0 +1,13 @@
+package jdlr.demo.escalade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
