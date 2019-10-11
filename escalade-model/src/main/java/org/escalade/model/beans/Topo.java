820f498424e30c6cb0be825e940289d3dac40683
JonathanDLR <marie89>
2019-10-11 14:32:17 +0200

    end mapping hibernate

--- escalade-model/src/main/java/org/escalade/model/beans/Topo.java
+++ escalade-model/src/main/java/org/escalade/model/beans/Topo.java
@@ -24,7 +24,7 @@ public class Topo {
 	@Column(name = "name")
 	private String name;
 	
-	@Column(name = "description")
+	@Column(name = "description", columnDefinition = "TEXT")
 	private String description;
 	
 	@ManyToMany(mappedBy = "topos")
